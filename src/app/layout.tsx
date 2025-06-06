5bb53d5d54777f5226214309a138632689852a2e
Ange230700 <kouakouangeericstephane@gmail.com>
2025-06-06 08:39:46 +0200

    fix: 🐛  repair A4 width dimension

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-[3508px] max-w-[2408px] overflow-hidden">
+    <html lang="en" className="h-[3508px] max-w-[2480px] overflow-hidden">
       <body
         className={`${geistSans.variable} ${geistMono.variable} flex h-full w-full flex-col overflow-hidden text-5xl/15 antialiased`}
       >
